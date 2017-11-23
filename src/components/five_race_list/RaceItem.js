77e4502d7e69b366603ab78f3db7c5b2cb7ecc2d
Unknown <294188719@qq.com>
2017-11-24 08:01:23 +1000

    feat: 5 race page desply 5 dummy race event

---
+++ src/components/five_race_list/RaceItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ListGroupItem} from 'reactstrap';
+
+
+export default class RaceItem extends React.Component {
+    render() {
+        return (
+                <ListGroupItem>
+                    dummy race
+                </ListGroupItem>
+        );
+    }
+}
